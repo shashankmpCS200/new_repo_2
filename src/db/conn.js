0e131568fb8a59e272d04088f24d6c9ec2629720
Shashank M Patil <117842648+shashankmpCS200@users.noreply.github.com>
2023-10-09 21:00:09 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // dotenv.config({path:'./config.env'})
 
 //const DB = process.env.DATABASE
-const DB = mongodb+srv://shashankmpcs21:Shanku%402002@cluster0.hltszt0.mongodb.net/?retryWrites=true&w=majority
+const DB = "mongodb+srv://shashankmpcs21:Shanku%402002@cluster0.hltszt0.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(DB).then(()=>{
     console.log("connection successful")
 }).catch((e)=>{
